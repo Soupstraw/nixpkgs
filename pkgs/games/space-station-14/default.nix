de944af9af2fcc71dab1106eaf800062071d7d91
Joosep Jääger <joosep.jaager@gmail.com>
2022-04-26 16:54:31 +0300

    Retain project structure

--- pkgs/games/space-station-14/default.nix
+++ pkgs/games/space-station-14/default.nix
@@ -32,10 +32,45 @@ buildDotnetModule rec {
   nugetDeps = ./deps.nix;
 
   dotnet-sdk = dotnetCorePackages.sdk_6_0;
-  dotnet-runtime = dotnetCorePackages.aspnetcore_6_0;
+  dotnet-runtime = dotnetCorePackages.runtime_6_0;
   installPath = "$out/opt/space-station-14";
+  buildType = "Release";
   executables = [];
-  patches = [./loaderenv.patch];
+  #dotnetBuildFlags = [
+  #  "-p:FullRelease=true"
+  #];
+  #patches = [./loaderenv.patch];
+  #dontDotnetConfigure = true;
+  #buildPhase = ''
+  #  dotnet publish SS14.Launcher/SS14.Launcher.csproj /p:FullRelease=True -c Release --no-self-contained -r linux-x64 /nologo
+  #  dotnet publish SS14.Loader/SS14.Loader.csproj -c Release --no-self-contained -r linux-x64 /nologo
+
+  #  # Create intermediate directories.
+  #  mkdir -p bin/publish/Linux/bin
+  #  mkdir -p bin/publish/Linux/bin/loader
+  #  mkdir -p bin/publish/Linux/dotnet
+
+  #  cp PublishFiles/SS14.Launcher PublishFiles/SS14.desktop bin/publish/Linux/
+  #  cp SS14.Launcher/bin/Release/net6.0/linux-x64/publish/* bin/publish/Linux/bin/
+  #  cp SS14.Loader/bin/Release/net6.0/linux-x64/publish/* bin/publish/Linux/bin/loader
+  #  cp -r Dependencies/dotnet/linux/* bin/publish/Linux/dotnet/
+  #'';
+  installPhase = ''
+    ls
+    dotnet publish SS14.Launcher/SS14.Launcher.csproj /p:FullRelease=True -c Release --no-self-contained --p:Deterministic=true --no-build /nologo
+    dotnet publish SS14.Loader/SS14.Loader.csproj -c Release --no-self-contained --p:Deterministic=true --no-build /nologo
+    # Create intermediate directories.
+    mkdir -p bin/publish/Linux/bin
+    mkdir -p bin/publish/Linux/bin/loader
+    mkdir -p bin/publish/Linux/dotnet
+
+    ls SS14.Launcher/bin/Release/net6.0/publish
+    cp -r SS14.Launcher/bin/Release/net6.0/publish/* bin/publish/Linux/bin/
+    cp -r SS14.Loader/bin/Release/net6.0/publish/* bin/publish/Linux/bin/loader
+
+    mkdir -p $out/lib/space-station-14
+    mv bin/publish/Linux/bin/* $out/lib/space-station-14/
+  '';
 
   dotnetFlags = [
     "-p:FullRelease=True"
@@ -46,7 +81,7 @@ buildDotnetModule rec {
     libICE
     libSM
     fontconfig
-    ss14-launcher
+    pkgs.bash
   ];
 
   meta = with lib; {
