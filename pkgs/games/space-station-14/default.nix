f03b5455e8744ff815a0d2120ccffd3364f25384
Joosep Jääger <joosep.jaager@gmail.com>
2022-04-28 13:07:20 +0300

    Changed a lot of things around

--- pkgs/games/space-station-14/default.nix
+++ pkgs/games/space-station-14/default.nix
@@ -8,6 +8,8 @@
 , libSM
 , fontconfig
 , pkgs
+, mkNugetDeps
+, mkNugetSource
 }:
 let
   version = "0.13.0";
@@ -15,10 +17,17 @@ let
     SS14_PATH=$(nix-store -r $(which space-station-14))
     exec $SS14_PATH/space-station-14/SS14.Launcher
   '';
+  nugetDeps = ./deps.nix;
+  _nugetDeps = mkNugetDeps { name = "ss14-nuget-deps"; nugetDeps = import nugetDeps; };
+  nugetSource = mkNugetSource {
+    name = "ss14-nuget-source";
+    description = "A Nuget source with the dependencies for ss14";
+    deps = [ _nugetDeps ];
+  };
 in
 buildDotnetModule rec {
   pname = "space-station-14";
-  inherit version;
+  inherit version nugetDeps;
 
   src = fetchFromGitHub {
     owner = "space-wizards";
@@ -29,52 +38,35 @@ buildDotnetModule rec {
   };
 
   projectFile = "SS14.Launcher.sln";
-  nugetDeps = ./deps.nix;
+
+  patches = [];
 
   dotnet-sdk = dotnetCorePackages.sdk_6_0;
   dotnet-runtime = dotnetCorePackages.runtime_6_0;
   installPath = "$out/opt/space-station-14";
-  buildType = "Release";
   executables = [];
-  #dotnetBuildFlags = [
-  #  "-p:FullRelease=true"
-  #];
-  #patches = [./loaderenv.patch];
-  #dontDotnetConfigure = true;
-  #buildPhase = ''
-  #  dotnet publish SS14.Launcher/SS14.Launcher.csproj /p:FullRelease=True -c Release --no-self-contained -r linux-x64 /nologo
-  #  dotnet publish SS14.Loader/SS14.Loader.csproj -c Release --no-self-contained -r linux-x64 /nologo
-
-  #  # Create intermediate directories.
-  #  mkdir -p bin/publish/Linux/bin
-  #  mkdir -p bin/publish/Linux/bin/loader
-  #  mkdir -p bin/publish/Linux/dotnet
-
-  #  cp PublishFiles/SS14.Launcher PublishFiles/SS14.desktop bin/publish/Linux/
-  #  cp SS14.Launcher/bin/Release/net6.0/linux-x64/publish/* bin/publish/Linux/bin/
-  #  cp SS14.Loader/bin/Release/net6.0/linux-x64/publish/* bin/publish/Linux/bin/loader
-  #  cp -r Dependencies/dotnet/linux/* bin/publish/Linux/dotnet/
-  #'';
-  installPhase = ''
-    ls
-    dotnet publish SS14.Launcher/SS14.Launcher.csproj /p:FullRelease=True -c Release --no-self-contained --p:Deterministic=true --no-build /nologo
-    dotnet publish SS14.Loader/SS14.Loader.csproj -c Release --no-self-contained --p:Deterministic=true --no-build /nologo
-    # Create intermediate directories.
-    mkdir -p bin/publish/Linux/bin
-    mkdir -p bin/publish/Linux/bin/loader
-    mkdir -p bin/publish/Linux/dotnet
-
-    ls SS14.Launcher/bin/Release/net6.0/publish
-    cp -r SS14.Launcher/bin/Release/net6.0/publish/* bin/publish/Linux/bin/
-    cp -r SS14.Loader/bin/Release/net6.0/publish/* bin/publish/Linux/bin/loader
-
-    mkdir -p $out/lib/space-station-14
-    mv bin/publish/Linux/bin/* $out/lib/space-station-14/
+  dontDotnetConfigure = true;
+  buildPhase = ''
+    dotnet publish SS14.Launcher/SS14.Launcher.csproj \
+      /p:FullRelease=True \
+      -c Release \
+      --no-self-contained \
+      -r linux-x64 \
+      /nologo \
+      --source "${nugetSource}/lib"
+    dotnet publish SS14.Loader/SS14.Loader.csproj \
+      -c Release \
+      --no-self-contained \
+      -r linux-x64 \
+      /nologo \
+      --source "${nugetSource}/lib"
   '';
+  installPhase = ''
+    mkdir -p $out/lib/space-station-14/loader
 
-  dotnetFlags = [
-    "-p:FullRelease=True"
-  ];
+    cp -r SS14.Launcher/bin/Release/net6.0/linux-x64/publish/* $out/lib/space-station-14/
+    cp -r SS14.Loader/bin/Release/net6.0/linux-x64/publish/* $out/lib/space-station-14/loader
+  '';
 
   runtimeDeps = [
     libX11
